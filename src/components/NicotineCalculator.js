22877ca1e3f4f277f7a1fe64c9ff38d3fa003a18
Carlos Loureda <balanzeneto@gmail.com>
2019-08-01 04:19:53 +0200

    fix: Resubida imagen de portada mas ligera.

--- src/components/NicotineCalculator.js
+++ src/components/NicotineCalculator.js
@@ -6,8 +6,6 @@ import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { white } from "ansi-colors";
 
 /* El volumne que damos por defecto al nicokit */
 const NICOKIT_VOLUMEN = 10; //ml
